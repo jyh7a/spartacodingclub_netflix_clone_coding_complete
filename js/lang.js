d3293210b2b91b05bb8340edfde967ce2a149897
YoungHun Jeong <jyh7a@naver.com>
2021-05-07 06:05:43 +0900

    lang.js 내용 추가

---
+++ js/lang.js
@@ -0,0 +1,69 @@
+let langData = 'ko';
+
+$('.lang-selector').on('change', function () {
+    let region = $(this).val();
+
+    if (region == 'ko') {
+        location.reload();
+    } else if (region == 'en') {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/spartacodingclub_netflix_clon_coding/assets/lang/en.json',
+            success: function (data) {
+                langData = data;
+                changeLanguage(langData);
+                langSelectorSync(region);
+                changeHtmlLang(region);
+            }
+        })
+    }
+
+
+})
+const changeLanguage = function (langData) {
+
+    $('.lang').each(function (index, item) {
+        const key = $(item).text().trim();
+
+        for (const property in langData) {
+            if (langData[key]) {
+                $(item).html(langData[key]);
+            }
+        }
+    })
+
+}
+const langSelectorSync = (region) => {
+    if (region == 'ko') {
+        $('.lang-selector').val("ko").prop("selected", true);
+    } else if (region == 'en') {
+        $('.lang-selector').val("en").prop("selected", true);
+    }
+}
+const changeHtmlLang = function (region) {
+    if (region == 'ko') {
+        $("html").attr("lang", "ko")
+    } else {
+        $("html").attr("lang", "en")
+    }
+}
+
+// Change language according to lang attribute
+$(function () {
+    let region = $('.lang-selector').val();
+    if (region == 'ko') {
+    } else if (region == 'en') {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/spartacodingclub_netflix_clon_coding/assets/lang/en.json',
+            success: function (data) {
+                langData = data;
+                changeLanguage(langData);
+                langSelectorSync(region);
+                changeHtmlLang(region);
+            }
+        })
+    }
+});
